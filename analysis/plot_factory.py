5e986496a25795ad2b57e8cbf88be6772f7aef14
AlexanderSch12 <alexander.schoeters@gmail.com>
2024-02-10 11:09:57 +0000

    wpt_results + empty collector check

---
+++ analysis/plot_factory.py
@@ -0,0 +1,152 @@
+from bokeh.embed import file_html
+from bokeh.models import BasicTickFormatter, ColumnDataSource, HoverTool
+from bokeh.models.glyphs import Circle
+from bokeh.palettes import Iridescent23
+from bokeh.plotting import figure, output_file, show
+from bokeh.resources import CDN
+from bokeh.transform import factor_cmap
+from dotenv import load_dotenv
+
+from bci.database.mongo.mongodb import MongoDB
+from bci.evaluations.logic import PlotParameters
+
+
+class PlotFactory:
+
+    @staticmethod
+    def show_plot(params: PlotParameters, db: MongoDB, path=None):
+        plot = PlotFactory.__create_plot(params, db)
+        if plot is None:
+            return
+        if path:
+            output_file(path)
+        show(plot)
+
+    @staticmethod
+    def create_html_plot_string(params: PlotParameters, db: MongoDB) -> tuple[str, int]:
+        # Check for missing plot parameters
+        missing_parameters = []
+        if not params.mech_group:
+            missing_parameters.append('selected experiment')
+        if not params.target_mech_id:
+            missing_parameters.append('reproduction ID')
+        if not params.browser_name:
+            missing_parameters.append('browser')
+        if not params.database_collection:
+            missing_parameters.append('database collection')
+
+        if missing_parameters:
+            return (
+                f'No Gantt chart can be plotted because the following parameters are missing {", ".join(missing_parameters)}',
+                0
+            )
+
+        plot, nb_of_evaluations = PlotFactory.__create_plot(params, db)
+        if plot is None and nb_of_evaluations == 0:
+            return '<p>No datapoints found...</p>', 0
+        elif plot is None:
+            return None, nb_of_evaluations
+        html = file_html(plot, CDN)
+        return html, nb_of_evaluations
+
+    @staticmethod
+    def __create_plot(params: PlotParameters, db: MongoDB):
+        docs = db.get_documents_for_plotting(params)
+        if len(docs) == 0:
+            return None, 0
+        elif len(docs) == params.previous_nb_of_evaluations:
+            return None, params.previous_nb_of_evaluations
+
+        data = PlotFactory.__add_outcome_info(params, docs)
+
+        # Create a data source with task start and end times, task names, and task colors
+        source = ColumnDataSource(data=data)
+
+        # define a color map based on the 'version' column
+        color_map = factor_cmap('browser_version_str', Iridescent23, list(set(source.data['browser_version_str'])))
+
+        # Create a figure and add the task circles
+        plot = figure(
+            title='Gantt Chart with Points',
+            x_range=(min(source.data['revision_number']), max(source.data['revision_number'])),
+            y_range=['Error', 'Not reproduced', 'Reproduced'],
+            height=350,
+            width=700,
+            tools='xwheel_zoom,reset,pan',
+            active_scroll='xwheel_zoom')
+        plot.add_glyph(source, Circle(x='revision_number', y='outcome', fill_color=color_map, fill_alpha=0.8, size=15))
+
+        # Formatting
+        plot.xaxis[0].formatter = BasicTickFormatter(use_scientific=False)
+        hover = HoverTool(
+            tooltips=[
+                    ('Revision Number', '@revision_number'),
+                    ('Browser Version', '@browser_version'),
+                ]
+        )
+        plot.add_tools(hover)
+
+        return plot, len(docs)
+
+    @staticmethod
+    def __transform_to_bokeh_compatible(docs: list) -> dict:
+        new_docs = {}
+        for d in docs:
+            for key, value in d.items():
+                if key not in new_docs:
+                    new_docs[key] = []
+                new_docs[key].append(value)
+        return new_docs
+
+    @staticmethod
+    def __add_outcome_info(params: PlotParameters, docs: dict):
+        docs_with_outcome = []
+        target_mech_id = params.target_mech_id if params.target_mech_id else params.mech_group
+        
+        for doc in docs:
+            new_doc = {
+                'revision_number': doc['revision_number'],
+                'browser_version': int(doc['browser_version'].split('.')[0]),
+                'browser_version_str': doc['browser_version'].split('.')[0]
+            }
+            if doc['wpt']:
+                result = doc['results']['requests'][0]['wpt_result']
+                if result == True:
+                    new_doc['outcome'] = 'Not reproduced'
+                elif result == False:
+                    new_doc['outcome'] = 'Reproduced'
+                else:
+                    new_doc['outcome'] = 'Error'
+                docs_with_outcome.append(new_doc)
+            else:
+                requests_to_target = list(filter(lambda x: f'/report/?leak={target_mech_id}' in x['url'], doc['results']['requests']))
+                requests_to_baseline = list(filter(lambda x: '/report/?leak=baseline' in x['url'], doc['results']['requests']))
+
+                if doc['dirty'] or len(requests_to_baseline) == 0:
+                    new_doc['outcome'] = 'Error'
+                    docs_with_outcome.append(new_doc)
+                elif len(requests_to_target) > 0:
+                    new_doc['outcome'] = 'Reproduced'
+                    docs_with_outcome.append(new_doc)
+                else:
+                    new_doc['outcome'] = 'Not reproduced'
+                    docs_with_outcome.append(new_doc)
+
+        docs_with_outcome = PlotFactory.__transform_to_bokeh_compatible(docs_with_outcome)
+        return docs_with_outcome
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    MongoDB.connect()
+    db = MongoDB.get_instance()
+    params = PlotParameters(
+        'c487155',
+        'c487155',
+        'chromium',
+        'csp_chromium',
+        major_version_range=(40, 105),
+    )
+    PlotFactory.show_plot(params, db)
+    html = PlotFactory.create_html_plot_string(params, db)
+    print(html)
---
+++ bci/evaluations/custom/custom_evaluation.py
@@ -0,0 +1,124 @@
+import logging
+import os
+import socket
+from unittest import TestResult
+from bci.browser.configuration.browser import Browser
+
+from bci.configuration import Global
+from bci.evaluations.custom.custom_mongodb import CustomMongoDB
+from bci.evaluations.evaluation_framework import EvaluationFramework
+from bci.evaluations.logic import TestParameters
+from bci.http.collector import Collector
+
+logger = logging.getLogger(__name__)
+hostname = socket.gethostname()
+
+class CustomEvaluationFramework(EvaluationFramework):
+
+    db_class = CustomMongoDB
+
+    def __init__(self):
+        super().__init__()
+        self.tests_per_project = {}
+        self.tests = {}
+        self.initialize_tests_and_url_queues()
+        self.initialize_wpt_tests()
+
+
+    def initialize_wpt_tests(self):
+        wpt_path = "/home/test/web-platform-tests"
+        if not os.listdir(wpt_path):
+            return
+        subject_name = "content-security-policy"
+        subject_path =  os.path.join(wpt_path, subject_name)
+        url = "http://web-platform.test:8000"
+        url_subject = os.path.join(url,subject_name)
+        for test_type in os.listdir(subject_path):
+            test_type_path = os.path.join(subject_path, test_type)
+            url_test_type = os.path.join(url_subject, test_type)
+            if not os.path.isdir(test_type_path):
+                continue
+            project_name = "WPT CSP: " + test_type
+            self.tests_per_project[project_name] = {}
+            for test_file in os.listdir(test_type_path):
+                if test_file.endswith('.html'):
+                    url_test = os.path.join(url_test_type,test_file)
+                    url_test = url_test +'?remote_ip=' + hostname
+                    test_name = os.path.splitext(test_file)[0]
+                    self.tests_per_project[project_name][test_name] = [url_test]
+                    self.tests[test_name] = self.tests_per_project[project_name][test_name]
+                    
+
+    def initialize_tests_and_url_queues(self):
+        page_folder_path = Global.custom_page_folder
+        project_names = [name for name in os.listdir(page_folder_path) if os.path.isdir(os.path.join(page_folder_path, name))]
+        for project_name in project_names:
+            # Find tests in folder
+            project_path = os.path.join(page_folder_path, project_name)
+            self.tests_per_project[project_name] = {}
+            for test_name in os.listdir(project_path):
+                url_queue_file_path = os.path.join(project_path, test_name, 'url_queue.txt')
+                if os.path.isfile(url_queue_file_path):
+                    # If an URL queue is specified, it is parsed and used
+                    with open(url_queue_file_path) as file:
+                        self.tests_per_project[project_name][test_name] = file.readlines()
+                        self.tests[test_name] = self.tests_per_project[project_name][test_name]
+                else:
+                    # Otherwise, a default URL queue is used, based on the domain that hosts the main page
+                    test_folder_path = os.path.join(project_path, test_name)
+                    for domain in os.listdir(test_folder_path):
+                        main_folder_path = os.path.join(test_folder_path, domain, 'main')
+                        if os.path.exists(main_folder_path):
+                            self.tests_per_project[project_name][test_name] = [
+                                f'https://{domain}/{project_name}/{test_name}/main',
+                                'https://a.test/report/?leak=baseline'
+                            ]
+                            self.tests[test_name] = self.tests_per_project[project_name][test_name]
+
+
+    def perform_specific_evaluation(self, browser: Browser, params: TestParameters) -> TestResult:
+        logger.info(f'Starting test for {params}')
+        browser_version = browser.version
+        binary_origin = browser.get_binary_origin()
+
+        collector = Collector()
+        collector.start()
+
+        is_dirty = False
+        try:
+            url_queue = self.tests[params.mech_group]
+            for url in url_queue:
+                tries = 0
+                while tries < 3:
+                    tries += 1
+                    browser.visit(url)
+        except Exception as e:
+            logger.error(f'Error during test: {e}', exc_info=True)
+            is_dirty = True
+        finally:
+            collector.stop()
+            if not is_dirty:
+                if len([request for request in collector.requests if 'report/?leak=baseline' in request['url']]) == 0:
+                    is_dirty = True
+            if 'wpt' in params.database_collection:
+                    is_wpt = True
+                    if not collector.requests:
+                        collector.requests.append({
+                            'url': 'undefined',
+                            'wpt_result': 'Sanity check: no result received'
+                            })
+            result = {
+                'requests': collector.requests
+            }
+
+        logger.debug(f'collector requests = {collector.requests}')
+
+        return params.create_test_result_with(browser_version, binary_origin, result, is_dirty, is_wpt)
+
+    def get_mech_groups(self, project=None):
+        if project:
+            return sorted(self.tests_per_project[project].keys())
+        return sorted(self.tests_per_project.keys())
+
+    def get_projects(self) -> list[str]:
+        return sorted(list(self.tests_per_project.keys()))
